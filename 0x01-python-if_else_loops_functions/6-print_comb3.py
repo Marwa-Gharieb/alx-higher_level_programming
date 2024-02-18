bba8264014806f0a69ee60abea8c341f549b11a6
Ifonll <ghariebm762@gmail.com>
2024-02-18 21:09:45 +0200

    Task 6 - DONE

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for num1 in range(10):
-    for num2 in range(10):
-        if num2 > num1 and num1 != 8:
-            print('{}{}'.format(num1, num2), end=", ")
-print("{}{}".format(num1 - 1, num2))
+
+for index_1 in range(10):
+    for index_2 in range(index_1 + 1, 10):
+        print("{:d}{:d}".format(index_1, index_2),
+              end=(", " if index_1 < 8 or index_2 < 9 else "\n"))
