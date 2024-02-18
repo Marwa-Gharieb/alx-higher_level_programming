3cb819815fe9431acec826f335b320c373fda2c2
Marwa Gharieb <ghariebm762@gmail.com>
2024-02-18 19:15:55 +0200

    s

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-def printing_numbers(my_list[]):
-   for num in my_list:
-     print("{:d}".format(num))
+def printing_numbers(my_list):
+  for num in my_list:
+    print("{:d}".format(num))
+my_list = [1, 2, 3, 4, 5]
+printing_numbers(my_list)
