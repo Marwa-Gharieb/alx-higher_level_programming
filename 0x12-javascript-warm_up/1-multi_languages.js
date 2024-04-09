4b33eedf69ef563969aff925a91fb797beb13ef0
Ifonll <142951270+Ifonll@users.noreply.github.com>
2024-04-09 02:24:14 +0200

    Create 1-multi_languages.js
    New

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const numOne = 'C is fun';
+const numTwo = 'Python is cool';
+const numThree = 'JavaScript is amazing';
+console.log(numOne);
+console.log(numTwo);
+console.log(numThree);
