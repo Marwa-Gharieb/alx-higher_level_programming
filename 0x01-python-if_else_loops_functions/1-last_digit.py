ac996bbf1204370f803cba2d8ed28b2a8bbb7ab4
marwa <Ifonll>
2024-04-23 13:39:52 -0400

    a

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,11 +6,10 @@ lg = n[-1]
 str(lg)
 
 if (number > 5):
-    if ( number >= 0):
+    if (number >= 0):
         print ("Last digit of " + n + " is " + lg + " and is greater than 5")
-    elif ( number < 0):
-        print ("Last digit of " + n + " is -" + lg + " and is greater than 5")
-        
+    elif (number < 0):
+        print ("Last digit of " + n + " is -" + lg + " and is greater than 5")      
 elif (number == 0):
     print ("Last digit of " + n + " is " + lg + " and is 0")
 elif (number < 6 and not 0):
