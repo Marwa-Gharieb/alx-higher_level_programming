28046ec53f40e563c8d29d53134603cfe8a2fff0
marwa <Ifonll>
2024-04-23 13:42:10 -0400

    a

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,15 +7,14 @@ str(lg)
 
 if (number > 5):
     if (number >= 0):
-        print ("Last digit of " + n + " is " + lg + " and is greater than 5")
+        print("Last digit of " + n + " is " + lg + " and is greater than 5")
     elif (number < 0):
-        print ("Last digit of " + n + " is -" + lg + " and is greater than 5")      
+        print("Last digit of " + n + " is -" + lg + " and is greater than 5")      
 elif (number == 0):
-    print ("Last digit of " + n + " is " + lg + " and is 0")
+    print("Last digit of " + n + " is " + lg + " and is 0")
 elif (number < 6 and not 0):
     if (number > 0):
-        print ("Last digit of " + n + " is " + lg + " and is less than 6 and not 0")
+        print("Last digit of " + n + " is " + lg + " and is less than 6 and not 0")
     elif (number < 0):
-        print ("Last digit of " + n + " is -" + lg + " and is less than 6 and not 0")
-
+        print("Last digit of " + n + " is -" + lg + " and is less than 6 and not 0")
 
