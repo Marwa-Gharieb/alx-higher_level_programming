dd0553fbc51bbcbdef0a521c2a5be74f39ea0c85
marwa <Ifonll>
2024-04-23 13:30:56 -0400

    a

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-number_str = str(number)
-last_digit = number_str[-1]
-str(last_digit)
+n_str = str(number)
+lg = n_str[-1]
+str(lg)
 
-if(number > 5):
-    if( number >= 0):
-        print("Last digit of " + number_str + " is " + last_digit + " and is greater than 5")
-    elif( number < 0):
-        print("Last digit of " + number_str + " is -" + last_digit + " and is greater than 5")
+if (number > 5):
+    if ( number >= 0):
+        print ( "Last digit of " + n_str + " is " + lg + " and is greater than 5" )
+    elif ( number < 0):
+        print ( "Last digit of " + n_str + " is -" + lg + " and is greater than 5" )
         
-    
-elif(number == 0):
-    print("Last digit of " + number_str + " is " + last_digit + " and is 0")
-elif(number < 6 and not 0):
-    if(number > 0):
-        print("Last digit of " + number_str + " is " + last_digit + " and is less than 6 and not 0")
-    elif(number < 0):
-        print("Last digit of " + number_str + " is -" + last_digit + " and is less than 6 and not 0")
+elif (number == 0):
+    print ( "Last digit of " + n_str + " is " + lg + " and is 0" )
+elif (number < 6 and not 0):
+    if (number > 0):
+        print ( "Last digit of " + n_str + " is " + lg + " and is less than 6 and not 0" )
+    elif (number < 0):
+        print ( "Last digit of " + n_str + " is -" + lg + " and is less than 6 and not 0" )
 
 
