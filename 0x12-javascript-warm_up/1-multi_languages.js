9681825415402d0303ceb7d1cbb8eea5ae54ecc4
marwa <Ifonll>
2024-06-03 16:17:07 -0400

    hello

--- 0x12-javascript-warm_up/1-multi_languages.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const numOne = 'C is fun';
-const numTwo = 'Python is cool';
-const numThree = 'JavaScript is amazing';
-
-console.log(numOne);
-console.log(numTwo);
-console.log(numThree);
