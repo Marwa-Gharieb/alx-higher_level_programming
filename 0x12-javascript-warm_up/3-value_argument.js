cc18f100c902393608151a8dd7ba2c6dd3d680de
marwa <Ifonll>
2024-06-04 05:38:51 -0400

    a

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
-let args = process.argv
-let x =args.slice(2)
 
-if (!args[2]){
-    console.log('No argument')
-}
-else {
-    console.log(x)
+if (!process.argv[2]) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
 }
