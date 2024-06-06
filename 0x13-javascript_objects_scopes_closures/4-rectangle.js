a95de6b0c7888448235e0dd699c01fcf6d06b28b
marwa <Ifonll>
2024-06-06 12:40:52 -0400

    a

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (_w, _h) {
+    if (_w > 0 && _h > 0) {
+      this.width = _w;
+      this.height = _h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+module.exports = Rectangle;
