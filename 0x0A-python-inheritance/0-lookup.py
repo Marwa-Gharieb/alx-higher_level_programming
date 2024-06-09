0ed35e52c5471595b295b3fee19e3b87fec7087f
marwa <Ifonll>
2024-06-09 08:59:11 -0400

    a

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""""returns the list of available attributes and methods """
+
+
+def lookup(obj):
+    list = dir(obj)
+    return list
---
+++ 0x12-javascript-warm_up/objects.js
@@ -0,0 +1,6 @@
+let car = {
+    name:'BMW',
+    model:2020,
+    price:500000
+};
+console.log(car.name)
