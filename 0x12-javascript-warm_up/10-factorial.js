b4d8fc3bc2ada5213d7c7ee421debb7e051da444
marwa <Ifonll>
2024-07-02 11:28:27 -0400

    :)

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,19 +1,15 @@
 #!/usr/bin/node
-const x = process.argv[2];
-function getFact (x) {
-  let a, b;
-  for (let i = x; i > 1; i--) {
-    const a = i * (i - 1);
-    for (let j = x; j > 1; j--) {
-      const b = +a;
-    }
-  }
-  return b;
-}
-const y = getFact(x);
 
-if (!x) {
-  console.log('NaN');
-} else {
-  console.log();
+const n = parseInt(process.argv[2])
+
+function factorial(n){
+    if(n == 1 || !n){
+      return 1
+    }
+    else{
+      return n * factorial(n-1)
+    }
 }
+
+const num = factorial(n)
+console.log(num)
