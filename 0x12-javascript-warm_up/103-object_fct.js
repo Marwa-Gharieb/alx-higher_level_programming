bf0ea71faea6a29ba6623c52c78352af980ab3fc
marwa <Ifonll>
2024-07-03 07:57:03 -0400

    :)

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,16 +1,28 @@
 #!/usr/bin/node
-let myObject = {
+// #!/usr/bin/node
+// const myObject = {
+//   type: 'object',
+//   value: 12
+// };
+// console.log(myObject);
+// myObject.incr = function(){
+// this.value++;
+// };
+// myObject.incr();
+// console.log(myObject);
+// myObject.incr();
+// console.log(myObject);
+// myObject.incr();
+// console.log(myObject);
+//
+
+const myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
-myObject = {
-  type: 'object',
-  value: 12,
-  incr () {
-    this.value += 1;
-    return (this.value);
-  }
+myObject.incr = function () {
+  this.value++;
 };
 myObject.incr();
 console.log(myObject);
